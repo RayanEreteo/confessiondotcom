9695d8fe29a242b39b18705a5e07d7a772413cb0
RayanEreteo <rayabf5@gmail.com>
2024-01-17 20:12:08 +0100

    création des bouttons

---
+++ app/components/ConfessionViewer.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Box, Button, Flex, Heading, Text, Tooltip } from "@chakra-ui/react";
+
+function ConfessionViewer() {
+  return (
+    <Box>
+      <Flex
+        justifyContent={"center"}
+        alignItems={"center"}
+        textAlign={"left"}
+        flexDirection={"column"}
+      >
+        <Text
+          border={"solid 3px black"}
+          borderRadius={"10px"}
+          background={"white"}
+          color={"black"}
+          width={"400px"}
+          height={"200px"}
+        >
+          dfsdf
+        </Text>
+        <Flex ml={"6rem"} mt={"3rem"} className="confess-options" width={"60rem"} justifyContent={"space-around"}>
+          <Tooltip label={"Envoyer du soutien sans laisser de commentaire"}>
+            <Button colorScheme="red">Envoyer un coeur</Button>
+          </Tooltip>
+          <Tooltip label={"Laisser un commentaire qui sera envoyé anonymement a la personne concerné"}>
+            <Button colorScheme="blue">Envoyer un commentaire</Button>
+          </Tooltip>
+          <Tooltip label={"test"}>
+            <Button>Passez à la confession suivante</Button>
+          </Tooltip>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default ConfessionViewer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-import Image from 'next/image'
+import ConfessionViewer from './components/ConfessionViewer'
 
 export default function Home() {
   return (
     <main>
-      <Flex textAlign={"center"} width={"100vw"} h={"100vh"} className='main-container' color={"white"} justifyContent={"center"} alignItems={"center"}>
+      <Flex textAlign={"center"} width={"100vw"} h={"100vh"} className='main-container' color={"white"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
         <section className='main-content'>
-          <Flex className='main-content-container'>
+          <Flex className='main-content-container' flexDirection={"column"}>
             <Box className='main-title'>
               <Heading id='main-title' fontSize={"6rem"}>La Confession</Heading>
               <br />
               <Text fontWeight={"bolder"}>Confessez-vous et ouvrez-vous anonymement</Text>
             </Box>
+            <ConfessionViewer></ConfessionViewer>
           </Flex>
         </section>
       </Flex>
